11bf3bc040f3230f44cc627457f53a9f97f5ce7d
JuanEs <jchavespa@uninpahu.edu.co>
2023-10-29 17:28:06 -0500

    Agregado POSTC

--- postc.php
+++ postc.php
@@ -1,65 +1,68 @@
 <?php
     include "conexion.php";
-    $idcat = $_POST["idcat"];   
-    $Categoria = $_POST["categoria"];
-
-    if(isset($_POST["enviarc"])){
-
-        $consulta = "INSERT INTO categoria (Categoria) VALUES ('$Categoria');";
-        $result = consulta($consulta);
-
-        if ($result){
-            echo "Datos almacenados correctamente";
-        }
-    }
-
-    if(isset($_POST["buscarc"])){
-            
-        $consulta = "SELECT * FROM categoria;";
-        $result = consulta($consulta);
-
-        if($result){
-            echo"
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                            <td>Id</td>
-                            <td>Categoria</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                ";
-                            while($row = mysqli_fetch_array($result)){
-                                echo"<tr>";
-                                    echo"<td>".$row['Id']."</td>";                  
-                                    echo"<td>".$row['Categoria']."</td>";                               
-                                echo"</tr>";
-                                }
-                echo"
-                        </tbody>
-                    </table>
-                </div>";
-        }
-    }
-
-    if(isset($_POST["eliminarc"])){
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
         
-        $consulta = "DELETE FROM categoria WHERE Id = '$idcat';";    
-        $result = consulta($consulta);
+        $idcat = $_POST["idcat"];   
+        $Categoria = $_POST["categoria"];
 
-        if ($result){
-            echo "Datos eliminados correctamente";
+        if(isset($_POST["enviarc"])){
+
+            $consulta = "INSERT INTO categoria (Categoria) VALUES ('$Categoria');";
+            $result = consulta($consulta);
+
+            if ($result){
+                echo "Datos almacenados correctamente";
+            }
         }
-    }
 
-    if(isset($_POST["actualizarc"])){
+        if(isset($_POST["buscarc"])){
 
-        $consulta = "UPDATE categoria SET categoria = '$Categoria' WHERE Id = '$idcat';";    
-        $result = consulta($consulta);
+            $consulta = "SELECT * FROM categoria;";
+            $result = consulta($consulta);
 
-        if ($result){
-            echo "Datos actualizados correctamente";
+            if($result){
+                echo"
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                <td>Id</td>
+                                <td>Categoria</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                    ";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo"<tr>";
+                                        echo"<td>".$row['Id']."</td>";                  
+                                        echo"<td>".$row['Categoria']."</td>";                               
+                                    echo"</tr>";
+                                    }
+                    echo"
+                            </tbody>
+                        </table>
+                    </div>";
+            }
         }
-    }
+
+        if(isset($_POST["eliminarc"])){
+
+            $consulta = "DELETE FROM categoria WHERE Id = '$idcat';";    
+            $result = consulta($consulta);
+
+            if ($result){
+                echo "Datos eliminados correctamente";
+            }
+        }
+
+        if(isset($_POST["actualizarc"])){
+
+            $consulta = "UPDATE categoria SET categoria = '$Categoria' WHERE Id = '$idcat';";    
+            $result = consulta($consulta);
+
+            if ($result){
+                echo "Datos actualizados correctamente";
+            }
+        }
+    }    
 ?>
